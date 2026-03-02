490b7950697f1efcf7d5c6b1d052996e11d0f910
Michael <mikelikesnepling@gmail.com>
2026-03-02 20:40:35 +0200

    route fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@
 import { Routes } from '@angular/router';
 import { RadioPlayerComponent } from './radio/radio-player.component';
 
-export const routes: Routes = [
-    { path: 'radio', component: RadioPlayerComponent },
+export const routes: Routes = [  
     { path: '',   redirectTo: '/radio', pathMatch: 'full' },
+    { path: 'radio', component: RadioPlayerComponent },
 ]; // sets up routes constant where you define your routes
 
 
